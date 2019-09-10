4e157231bd718d398ae76fc407f7b78e3025b6d4
connormccafferty <connor.mccafferty@openfin.co>
2019-09-10 11:53:12 -0400

    working example

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# InterApplicationBus Close Child Windows Example
+# InterApplicationBus Close Child Windows 
+
+Simple example demonstrating a child appliction being launched from the main window and receiving a close message from the main application through the IAB.
+
+### How to use:
+
+* clone this repo: `https://github.com/connormccafferty/iab-window-close`
+* install dependancies: `cd iab-window-close && npm install`
+* start live server and launch the application: `npm start`
+
--- public/js/main.js
+++ public/js/main.js
@@ -14,21 +14,58 @@ async function init() {
     const app = await fin.Application.getCurrent();
     const win = await fin.Window.getCurrent();
 
-    const ofVersion = document.querySelector('#of-version');
-    ofVersion.innerText = await fin.System.getVersion();
+    openDevTools();
 
-    //Only launch new windows from the main window.
-    if (win.identity.name === app.identity.uuid) {
-        //subscribing to the run-requested events will allow us to react to secondary launches, clicking on the icon once the Application is running for example.
-        //for this app we will  launch a child window the first the user clicks on the desktop.
-        app.once('run-requested', async () => {
-            await fin.Window.create({
-                name: 'childWindow',
-                url: location.href,
-                defaultWidth: 320,
-                defaultHeight: 320,
-                autoShow: true
-            });
-        });
+    
+    // only launch / close children from the main window
+    if (win.identity.uuid === 'OpenfinPOC') {
+        // launch child app
+        launchChildApp();
+
+        const closeBtn = document.querySelector('#close-windows');
+
+        closeBtn.addEventListener('click', event => {
+            event.preventDefault();
+            closeChildWindows();
+            let challenge = confirm('Are you sure?');
+            if (challenge) {
+                closeChildWindows(challenge);
+                win.close(true);
+            } else {
+                console.log('Confirm was negative.')
+            };
+        })
     }
+
+    // subscribe to close windows and listen close message from child app
+    if (win.identity.uuid === 'OpenfinPOC-child') {
+        fin.InterApplicationBus.subscribe({ uuid: 'OpenfinPOC' }, 'close-windows', sub_msg => {
+            // do close processing here
+            if (sub_msg) {
+                // interval / timeout to demonstrate message being received from the main application
+                let countdown = 3;
+                setInterval(() => {
+                    if (countdown === 3) console.log(`Closing in ${countdown}...`);
+                    else console.log(`${countdown}...`);
+                    countdown--;
+                }, 1000);
+                setTimeout(() => win.close(true), 4000);
+            }
+        }).then(() => console.log('Subscribed to close windows.')).catch(err => console.log(err));
+    }
+}
+
+function openDevTools() {
+    const { identity } = fin.Window.getCurrentSync();
+    fin.System.showDeveloperTools(identity);
+}
+
+async function launchChildApp() {
+    return fin.Application.startFromManifest('http://localhost:5555/child_app.json');  
+}
+
+function closeChildWindows(challenge) {
+    fin.InterApplicationBus.publish('close-windows', challenge)
+        .then(() => console.log('Published close windows message.'))
+        .catch(err => console.log(err));
 }
